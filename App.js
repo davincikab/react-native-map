69efc4b72009857957585db951fab64873e97c7f
davincikab <davidnganganjeri079@gmail.com>
2020-07-27 22:43:47 +0300

    updating map size

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("pk.eyJ1IjoiZGF1ZGk5NyIsImEiOiJjanJtY3B1bjYwZ3F2NGFvOXZ1a29iMmp6In0.9ZdvuGInodgDk7cv-KlujA");
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  container: {
+    height: 650,
+    width: 450,
+    backgroundColor: "tomato"
+  },
+  map: {
+    flex: 1
+  }
+});
+
+export default class App extends Component {
+  componentDidMount() {
+    MapboxGL.setTelemetryEnabled(false);
+  }
+
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} />
+        </View>
+      </View>
+    );
+  }
+}
